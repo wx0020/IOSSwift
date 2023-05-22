e826a2b0cdf788d5df07664ad9206ea01ee857bc
wuxiang <xiangw@itron.com.cn>
2023-05-22 15:46:39 +0800

    init

---
+++ KanMeTo/KanMeTo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanMeTo
+//
+//  Created by 翔吴 on 2023/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
